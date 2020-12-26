29c645b593b44ac33769b9e5232a53aa00adb555
jorgechauxjr <chauxjorge@hotmail.com>
2020-12-26 14:53:44 -0500

    Add 1-destrucring.js

---
+++ js-fundamentals-to-functional/0x00-objects/1-destrucring.js
@@ -0,0 +1,11 @@
+// Create an object that looks like this:
+// {"name": "Rusty", "room":"ktchen", "weapon": "cadestick"}
+// extract out the weapon and location using destructuring
+
+const {name, weapon, room} =  {"name": "Rusty", "room":"ktchen", "weapon": "cadestick"}
+
+// the same as
+// const obj =  {"name": "Rusty", "room":"ktchen", "weapon": "cadestick"}
+// const name = obj.name
+// const room = obj.room
+// const weapon = obj.weapon
