84a71d45af0076689a5aaff15de18020c85db2d9
jorgechauxjr <chauxjorge@hotmail.com>
2020-12-26 14:52:56 -0500

    Rename game chacter

---
+++ js-fundamentals-to-functional/0x00-objects/0-gameCharacters.js
@@ -0,0 +1,17 @@
+// Create an object using braket and dot notation that represents the
+// characters and related data you may find in a game of Clue.
+
+var game = {};
+
+game.murderer = "??";
+
+game['weapons'] = [
+  {type: 'lasers', location: 'lab'},
+  {type: 'angry cats', location: 'house'},
+  {type: 'knife', location: 'kitchen'},
+];
+
+game.name = [];
+game.name[0] = 'Miss Scarlet';
+game.name.push('Mr. Green')
+// ['Miss Scarlet', 'Mr. Green']
